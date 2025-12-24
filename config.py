7fbbb3bab7bc724ee7915ae2fc321b5745b4fc94
aleksejg <a1ex.g@list.ru>
2025-12-24 11:17:44 +0300

    add handlers

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ import os
 dotenv.load_dotenv()
 
 BOT_TOKEN = os.getenv('TOKEN')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+PROXY = os.getenv('PROXY')
+ADMIN_ID = int(os.getenv('ADMIN_ID'))
--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
-from aiogram.types import Message
 import asyncio
+from aiogram import Bot, Dispatcher
+from handlers import main_router
 import config
+import misc
 
 bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher()
 
 
-@dp.message(Command('start'))
-async def start_command(message: Message):
-    print(message)
-
-
-@dp.message()
-async def all_messages(message: Message, bot: Bot):
-    msg_text = f'User {message.from_user.full_name} sent message: \n{message.text}'
-    await message.answer(text=msg_text)
-    # await bot.send_message(chat_id=, text=msg_text)
-
-
 async def stat_bot():
+    dp.startup.register(misc.on_start)
+    dp.shutdown.register(misc.on_shutdown)
+    dp.include_router(main_router)
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    asyncio.run(stat_bot())
+    try:
+        asyncio.run(stat_bot())
+    except KeyboardInterrupt:
+        pass
